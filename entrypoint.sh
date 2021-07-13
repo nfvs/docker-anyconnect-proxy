093dfccebf1fe3edd898c48ccaece0706484232a
Nuno Santos <nunofvsantos@gmail.com>
2021-07-13 11:57:59 +0100

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -x
+
 # Fill in hostname in csd-wrapper file
 sed -i "s/^CSD_HOSTNAME=.*$/CSD_HOSTNAME=${ANYCONNECT_HOST}/" /csd-wrapper.sh
 
-( echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --authgroup=$ANYCONNECT_GROUP --timestamp --passwd-on-stdin
+( echo "$ANYCONNECT_PASSWORD" ) | openconnect "$ANYCONNECT_SERVER" --user="$ANYCONNECT_USER" --authgroup="$ANYCONNECT_GROUP" --timestamp --passwd-on-stdin --servercert "$ANYCONNECT_CERT"
--- service.sh
+++ service.sh
@@ -3,28 +3,35 @@
 CODE_PATH="$HOME/.docker-anyconnect"
 
 # Add password to mac keychain: security add-generic-password -a <user> -s <service> -w
-ANYCONNECT_PASSWORD=$(security find-generic-password -a <user> -s <service> -w)
+ANYCONNECT_PASSWORD=$(security find-generic-password -a $(id -un) -s ngvpn.nvidia.com -w)
 export ANYCONNECT_PASSWORD
 
-while [[ $# -gt 0 ]]
+DOCKER_COMPOSE="docker-compose"
+# if [ -n "$(docker compose)" ]; then
+#     DOCKER_COMPOSE="docker compose"
+# fi
+
+DOCKER_COMPOSE_UP_ARGS="--detach"
+
+while [ "$#" -gt 0 ]
 do
 key="$1"
 
 case $key in
     on|up)
-    (cd "$CODE_PATH" && docker-compose up -d)
+    (cd "$CODE_PATH" && ${DOCKER_COMPOSE} up ${DOCKER_COMPOSE_UP_ARGS})
     shift
     ;;
     off|down)
-    (cd "$CODE_PATH" && docker-compose down)
+    (cd "$CODE_PATH" && ${DOCKER_COMPOSE} down)
     shift
     ;;
     restart)
-    (cd "$CODE_PATH" && docker-compose restart)
+    (cd "$CODE_PATH" && docker restart ngvpn_vpn)
     shift
     ;;
     ps|status)
-    (cd "$CODE_PATH" && docker-compose ps)
+    (cd "$CODE_PATH" && ${DOCKER_COMPOSE} ps)
     shift
     ;;
     *)    # unknown option
